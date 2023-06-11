bf2b54253bba571bb4ac43d4c1d3933c1a5266a4
Jagjit Singh <jagik22@gmail.com>
2023-06-11 09:56:43 -0700

    add screen

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,23 @@
+import { useState } from "react";
+import { Grid } from "@mui/material";
+
 import Screen from "./components/Screen";
 import FullGrid from "./components/Grids/FullGrid";
 import cssClasses from "./App.module.css";
 
 function App() {
-  const { box } = cssClasses;
+  const [screenValue, setScreenValue] = useState("0");
+  const { box, grid } = cssClasses;
   return (
     <div className={box}>
-      <Screen />
-      <FullGrid />
+      <Grid sx={{ width: "18em" }} container columns={1} spacing={0.5}>
+        <Grid className={grid} item xs={1}>
+          <Screen value={screenValue} />
+        </Grid>
+        <Grid className={grid} item xs={1}>
+          <FullGrid />
+        </Grid>
+      </Grid>
     </div>
   );
 }
--- frontend/src/components/Screen.tsx
+++ frontend/src/components/Screen.tsx
@@ -1,5 +1,15 @@
-const Screen = () => {
-  return <></>;
+import { Grid, Typography } from "@mui/material";
+import cssClasses from "./Screen.module.css";
+
+const Screen: React.FC<{ value: string }> = ({ value }) => {
+  const { screen, text } = cssClasses;
+  return (
+    <Grid className={screen} item xs={1}>
+      <Typography className={text} variant="h4">
+        {value}
+      </Typography>
+    </Grid>
+  );
 };
 
 export default Screen;
